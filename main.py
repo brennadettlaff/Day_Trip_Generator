e70b63dd082e71dd76e3644d3cad40465ec27c41
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-08-08 12:49:59 -0600

    Added match_list Function to Select From Other Lists

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 ##List of locations
+from tkinter import Y
+
+
 destinations = ["Denver", "Aspen", "Colorado Springs", "Glenwood Springs", "Durango", "Morrison", "Estas Park", "Grand Junction"]
 
 ##List of transportation
@@ -24,6 +27,7 @@ colosprings_restaurants = ["Cowboy Star", "Four by Brother Luck","The Margarita"
 glensprings_restaurants = ["CO Ranch House", "Vicco's Charcoalburger Drive-In", "Slope & Hatch", "19th Street Diner", "Glenwood Canyon Brewpub"]
 durango_restaurants = ["Derailed Pour House", "Rice Monkeys", "El Moro Tavern", "Diamond Belle Saloon", "Steamworks Brewing Company"]
 morrison_restaurants = ["The Fort Resaurant", "Morrison Inn", "Red Rocks Grill", "Sit N Bull Saloon", "Twin Forks Tavern"]
+estaspark_restaurants = ["Rock Inn Mountain Tavern", "Smokin' Dave's BBQ & Brew", "Bird & Jim", "Twin Owls Steakhouse", "Dunraven"]
 grandjunct_restaurants = ["Bin 707 Foodbar", "il Bistro Italiano", "Kannah Creek Brewing Company", "Rockslide Restaurant", "Pufferbelly Station"]
 
 
@@ -37,24 +41,43 @@ def select_item(list_to_use, type_of_item):
     return generated_item 
 
 def user_confirmation(list_to_use, type_of_item):
-    select_item(list_to_use, type_of_item)
+    selected_item = select_item(list_to_use, type_of_item)
     while True:
         user_input = input(f"Is this an acceptible {type_of_item}? (enter y/n): ")
         if user_input == "y" or user_input == "Y":
             print("Perfect, lets move on.")
             break
         elif user_input == "n" or user_input == "N":
-            select_item(list_to_use, type_of_item)                     
+            print(f"We will select a different {type_of_item} for you.")
+            selected_item = select_item(list_to_use, type_of_item)                    
         else:
             print("Please answer y or yes or n for no.")
 
-def select_other(trans_list, ent_list, rest_list):              ##combine selections for transportation, entertainment, and restauraunts
+    return selected_item
+
+def select_other(trans_list, ent_list, rest_list):              
     user_confirmation(trans_list, "transportation")
     user_confirmation(ent_list, "entertainment")
     user_confirmation(rest_list, "restaurant")
 
+def match_lists(selected_destination):
+    if selected_destination == "Denver":                 
+        select_other(transportation_modes_all, denver_entertainment, denver_restaurants)
+    elif selected_destination == "Aspen":
+        select_other(transportation_modes_plane, aspen_entertainment, aspen_restaurants)
+    elif selected_destination == "Colorado Springs":
+        select_other(transportation_modes_plane, colosprings_entertainment, colosprings_restaurants)
+    elif selected_destination == "Glenwood Springs":
+        select_other(transportation_modes_train, glensprings_entertainment, glensprings_restaurants)
+    elif selected_destination == "Durango":
+        select_other(transportation_modes_plane, durango_entertainment, durango_restaurants)
+    elif selected_destination == "Morrison":
+        select_other(transportation_modes_general, morrison_entertainment, morrison_restaurants)
+    elif selected_destination == "Estas Park":
+        select_other(transportation_modes_general, estaspark_entertainment, estaspark_restaurants)
+    elif selected_destination == "Estas Park":
+        select_other(transportation_modes_general, estaspark_entertainment, estaspark_restaurants)
 
-user_confirmation(destinations, "destinations")
-select_other(transportation_modes_all, denver_entertainment, denver_restaurants)
-
+select_destination = user_confirmation(destinations, "destination")
+match_lists(select_destination)
 
